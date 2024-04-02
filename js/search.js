59c0948e5648004b38064c75244f379b26471c99
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-02 14:13:26 +0200

    corect with style search item

---
+++ js/search.js
@@ -0,0 +1,9 @@
+const searchItem = document.querySelector('.navigation-search-link');
+
+const onOpenSearch = () => {
+  console.log(onOpenSearch);
+};
+searchItem.addEventListener('click', onOpenSearch);
+// navigation-search-container;
+
+console.log(2);
