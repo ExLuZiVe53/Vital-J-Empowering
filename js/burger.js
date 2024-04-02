a8d8d8b8d4382fc6dc9dbd063db6072e0fa0b895
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-02 13:05:08 +0200

    added block scrol open modal.js

--- js/burger.js
+++ js/burger.js
@@ -11,3 +11,22 @@ const onMenuCloseClick = () => {
 };
 burgerBtn.addEventListener('click', onClick);
 burgerBtnClose.addEventListener('click', onMenuCloseClick);
+
+// Modal scroll
+document.body.style.position = 'fixed';
+document.body.style.top = `-${window.scrollY}px`;
+
+document.body.style.position = '';
+document.body.style.top = '';
+
+const scrollY = document.body.style.top;
+document.body.style.position = '';
+document.body.style.top = '';
+window.scrollTo(0, parseInt(scroll || '0') * -1);
+
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty(
+    '--scroll-y',
+    `${window.scrollY}px`
+  );
+});
