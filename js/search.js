0a5bc822b0f55fdd9ebbc59314c0de149357eba5
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-05 15:43:02 +0200

    added modal-search js

--- js/search.js
+++ js/search.js
@@ -1,6 +1,22 @@
-const searchItem = document.querySelector('.navigation-search-link');
-const searchContainer = document.querySelector('.navigation-search-container');
+const searchBtn = document.querySelector('.navigation-search-link');
+const searchContainer = document.querySelector('.modal-search-wrapper');
+const inputSearch = document.querySelector('.modal-search');
 
-const onOpenSearch = () => {};
-searchItem.addEventListener('click', onOpenSearch);
-// navigation-search-container;
+const onOpenBtn = () => {
+  searchBtn.classList.add('is-open');
+  searchContainer.classList.add('no-hidden');
+};
+const onHideSearch = event => {
+  //   console.log('code:', event.code);
+  if (event.code === 'Escape') {
+    event.preventDefault();
+    searchContainer.classList.remove('no-hidden');
+  }
+};
+const onInput = event => {
+  console.log(event.target.value);
+};
+
+searchBtn.addEventListener('click', onOpenBtn);
+searchContainer.addEventListener('keyup', onHideSearch);
+inputSearch.addEventListener('input', onInput);
