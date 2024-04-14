8c475a23f9a9734c341b1ef4c05819574947ea20
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-14 08:06:16 +0200

    unfortunately remove on odd code

--- js/tabs.js
+++ js/tabs.js
@@ -1,16 +1,7 @@
 const activeBtn = document.querySelector('.btnArrow');
-const formSub = document.querySelector('.formSubmit');
 
 const onActiveClick = event => {
   activeBtn.classList.toggle('btnRotate');
-
-  //   rotateArrow.classList.toggle('.rotate');
-};
-
-const onFormChange = event => {
-  console.dir(event.target.checked);
-  console.log(event.target.value);
 };
 
 activeBtn.addEventListener('click', onActiveClick);
-formSub.addEventListener('change', onFormChange);
