e3f0657efedeb641ae288349587f67842b10a363
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-19 22:03:50 +0200

    added form.js

---
+++ js/my-account-form.js
@@ -0,0 +1,9 @@
+const myForm = document.querySelector('.js-myFormData');
+console.log(myForm);
+
+const onInputChange = event => {
+  console.log(event.target.value);
+  console.log(event.target.name);
+};
+
+myForm.addEventListener('input', onInputChange);
