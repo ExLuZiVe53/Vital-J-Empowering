37ffc52cc024bde954aeca8d3b962705e698d900
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-21 22:28:19 +0200

    Update my-account-form.js

--- js/my-account-form.js
+++ js/my-account-form.js
@@ -2,8 +2,8 @@ const myForm = document.querySelector('.js-myFormData');
 console.log(myForm);
 
 const onInputChange = event => {
-  console.log(event.target.value);
-  console.log(event.target.name);
+  // console.log(event.target.value);
+  // console.log(event.target.name);
 };
 
 myForm.addEventListener('input', onInputChange);
