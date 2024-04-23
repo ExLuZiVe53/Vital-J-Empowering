eb2d89630f5ac105111651f14f02737d6eb86102
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-23 06:29:20 +0200

    added addEventListener from keyup

--- js/modal-credit-card.js
+++ js/modal-credit-card.js
@@ -1,14 +1,20 @@
 const modalCreditCard = document.querySelector('.modal-credit-card');
 const openModal = document.querySelector('.btn-icon');
+const btnClose = document.querySelector('.btn-close');
 
 const onModalBackdrop = () => {
-  modalCreditCard.classList.toggle('js-modal');
+  modalCreditCard.classList.add('js-modal');
 };
 
 const onOpenModal = () => {
   openModal.classList.toggle('js-open-modal');
-  modalCreditCard.classList.toggle('js-modal');
+  modalCreditCard.classList.add('js-modal');
+};
+
+const onCloseModal = event => {
+  console.log('Click for btn-close', event.code);
 };
 
 modalCreditCard.addEventListener('click', onModalBackdrop);
 openModal.addEventListener('click', onOpenModal);
+document.addEventListener('keyup', onCloseModal);
