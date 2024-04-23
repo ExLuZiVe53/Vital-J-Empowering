1f268be230b5261ffb63cea30d0feafda3698432
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-23 06:28:34 +0200

    remove console.log(event)

--- js/my-account-form.js
+++ js/my-account-form.js
@@ -1,9 +1,8 @@
 const myForm = document.querySelector('.js-myFormData');
-console.log(myForm);
 
 const onInputChange = event => {
-  console.log(event.target.value);
-  console.log(event.target.name);
+  // console.log(event.target.value);
+  // console.log(event.target.name);
 };
 
 myForm.addEventListener('input', onInputChange);
