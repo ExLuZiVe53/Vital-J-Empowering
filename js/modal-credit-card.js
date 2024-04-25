e14479c75ff0f2788e5639124d46f86a53272d35
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-25 22:20:19 +0200

    added btn-cross.js

--- js/modal-credit-card.js
+++ js/modal-credit-card.js
@@ -1,19 +1,21 @@
 const modalCreditCard = document.querySelector('.modal-credit-card');
 const openModal = document.querySelector('.btn-icon');
+const btnCross = document.querySelector('.btn-cross');
 
 const onModalBackdrop = () => {
   modalCreditCard.classList.add('js-modal');
 };
 
 const onOpenModal = () => {
-  openModal.classList.toggle('js-open-modal');
+  openModal.classList.add('js-open-modal');
   modalCreditCard.classList.add('js-modal');
 };
 
-const onCloseModal = event => {
-  console.log(event.currentTarget);
+const onCloseModal = () => {
+  console.log('click cross');
+  modalCreditCard.classList.remove('js-modal');
 };
 
 modalCreditCard.addEventListener('click', onModalBackdrop);
 openModal.addEventListener('click', onOpenModal);
-burgerBtnClose.addEventListener('click', onCloseModal);
+btnCross.addEventListener('click', onCloseModal);
