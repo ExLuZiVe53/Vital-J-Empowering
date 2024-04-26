cd9131a3162fd33d94499727e40586bec584a9a1
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-26 21:46:10 +0200

    corect modal-credit-card.js
    Dell class js-open-mdal,
    delete function onModalBackdrop

--- js/modal-credit-card.js
+++ js/modal-credit-card.js
@@ -2,10 +2,6 @@ const modalCreditCard = document.querySelector('.modal-credit-card');
 const openModal = document.querySelector('.btn-icon');
 const btnCross = document.querySelector('.btn-cross');
 
-const onModalBackdrop = () => {
-  modalCreditCard.classList.add('js-modal');
-};
-
 const onOpenModal = () => {
   openModal.classList.add('js-open-modal');
   modalCreditCard.classList.add('js-modal');
@@ -14,8 +10,8 @@ const onOpenModal = () => {
 const onCloseModal = () => {
   console.log('click cross');
   modalCreditCard.classList.remove('js-modal');
+  openModal.classList.remove('js-open-modal');
 };
 
-modalCreditCard.addEventListener('click', onModalBackdrop);
 openModal.addEventListener('click', onOpenModal);
 btnCross.addEventListener('click', onCloseModal);
