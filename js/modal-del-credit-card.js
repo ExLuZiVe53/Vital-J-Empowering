9e8760f4b1c4ec7f4a9040190773126dba106f4a
ExLuZiVe53 <imstudent27@gmail.com>
2024-04-30 19:32:08 +0200

    added method from close "btn Escape"

--- js/modal-del-credit-card.js
+++ js/modal-del-credit-card.js
@@ -1,18 +1,28 @@
-const modalDellOpenBtn = document.querySelector('.icon-basket');
+const modalDellOpenBtn = document.querySelector('.icon-basket-card');
 const openModalDell = document.querySelector('.modal-credit-card.del');
-const JsCloseNo = document.querySelector('.js-close-no');
+// const clickBtnNo = document.querySelector('.js-close-no');
+const clickBackdrop = document.querySelector('.modal-backdrop.open');
 
 const onOpenDell = () => {
   modalDellOpenBtn.classList.add('js-open-modal');
   openModalDell.classList.add('js-open');
 };
 
-const onJsCloseNo = event => {
+const onJsCloseEsc = event => {
   if (event.code === 'Escape') {
     modalDellOpenBtn.remove('js-open-modal');
     openModalDell.remove('js-open');
   }
 };
 
+const onClickBackdrop = event => {
+  if (event.target.nodeName === 'BUTTON') {
+    modalDellOpenBtn.remove('js-open-modal');
+    openModalDell.remove('js-open');
+  }
+  console.log(event.target);
+};
+
 modalDellOpenBtn.addEventListener('click', onOpenDell);
-document.addEventListener('keydown', onJsCloseNo);
+document.addEventListener('keydown', onJsCloseEsc);
+clickBackdrop.addEventListener('click', onClickBackdrop);
