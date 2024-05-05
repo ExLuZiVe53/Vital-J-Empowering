db230b0eb42cb511b8fc52f6a6e0d6b92d51ea08
ExLuZiVe53 <imstudent27@gmail.com>
2024-05-05 20:03:46 +0200

    added functions from tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -1,7 +1,14 @@
 const activeBtn = document.querySelector('.btnArrow');
+const hideList = document.querySelector('.interface-list__one-item');
 
 const onActiveClick = event => {
   activeBtn.classList.toggle('btnRotate');
+  hideList.classList.toggle('hidden');
+};
+
+const onHiddenList = () => {
+  console.log('click hideList');
 };
 
 activeBtn.addEventListener('click', onActiveClick);
+hideList.addEventListener('click', onHiddenList);
