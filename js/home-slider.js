d5abb8c19190c8e1e45ee34e8e93ba1f08c2dbdf
ExLuZiVe53 <imstudent27@gmail.com>
2024-08-12 22:09:51 +0200

    Update home-slider.js

--- js/home-slider.js
+++ js/home-slider.js
@@ -3,13 +3,13 @@ const arrowRightBtn = document.querySelector(
   '.card-wrapper__button-arrow.right'
 );
 const cardContainer = document.querySelector('.card-container');
-// console.log(cardContainer);
+console.log(cardContainer);
 
 const onArrowLeftBtn = event => {
-  // console.log('click left');
+  console.log('click slider button left');
 };
 const onArrowRightBtn = event => {
-  // console.log('click right');
+  console.log('click slider button right');
 };
 
 arrowLeftBtn.addEventListener('click', onArrowLeftBtn);
