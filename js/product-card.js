de775e950021b1f9e3b7849cdcdd98252024acbc
ExLuZiVe53 <imstudent27@gmail.com>
2024-09-18 20:17:18 +0200

    added markup butoon from js

--- js/product-card.js
+++ js/product-card.js
@@ -1,6 +1,42 @@
 const activeTab = document.querySelector('.buttons-select');
+
+const dataActions = ['Description', 'Compound', 'Use'];
+const markup = dataActions
+  .map(
+    (actionName, index) => `
+  <button
+  class="buttons-select__tab"
+  type="radio"
+  data-action=${index}
+              >${actionName}
+              </button>`
+    //   <button
+    //   class="buttons-select__tab"
+    //   type="radio"
+    //   data-action="active"
+    // >
+    //   Description
+    // </button>
+    // <button
+    //   class="buttons-select__tab"
+    //   type="radio"
+    //   data-action="noActive"
+    // >
+    //   Compound
+    // </button>
+    // <button
+    //   class="buttons-select__tab"
+    //   type="radio"
+    //   data-action="visually"
+    // >
+    //   Use
+    // </button>
+  )
+  .join('');
 const buttonSelect = document.querySelector('.buttons-select__tab');
 console.log(buttonSelect);
+
+activeTab.insertAdjacentHTML('beforeend', markup);
 
 const onActiveTab = () => {
   activeTab.classList.toggle('addClass');
