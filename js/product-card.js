22139a939831093d6479c204e9726b22e8b59680
ExLuZiVe53 <imstudent27@gmail.com>
2025-05-09 21:01:10 +0200

    Update product-card.js

--- js/product-card.js
+++ js/product-card.js
@@ -1,31 +1,45 @@
 const activeTab = document.querySelector('.buttons-select');
-const activeButton = document.querySelectorAll('.buttons-select__tab');
+const activeButton = document.querySelector('.button-select__tab');
 
 const dataActions = ['Description', 'Compound', 'Use'];
-const markup = dataActions
-  .map((actionName, index) => markupButtons(actionName, index))
-  .join('');
-activeTab.insertAdjacentHTML('beforeend', markup);
+// const markup = dataActions
+//   .map((actionName, index) => {
+//     return `<button
+//             class="buttons-select__tab" type="radio" data-action=${index}>
+//             ${actionName}
+//           </button>`;
+//   })
+//   .join('');
+// activeTab.innerHTML = markup;
+createButton();
 
-function markupButtons(actionName, index) {
-  return `<button
-  class="buttons-select__tab"
-  type="radio"
-  data-action=${index}>${actionName}</button>`;
+function createButton() {
+  const items = [];
+  for (let i = 0; i < dataActions.length; i += 1) {
+    const item = document.createElement('button');
+    item.type = radio;
+    item.classList.add('button-select__tab');
+    items.push(item);
+  }
+  activeTab.append(...items);
 }
+console.log(activeButton);
 
 const buttonHandler = event => {
   console.log(event.target);
   console.log(event.currentTarget);
-  if (event.currentTarget !== event.target) {
+  if (event.currentTarget === event.target) {
     switch (event.target.dataset.action) {
       case '0':
+        activeButton.classlist.add('active');
         console.log('Zero');
         break;
       case '1':
+        activeButton.classlist.add('active');
         console.log('First');
         break;
       case '2':
+        activeButton.classlist.add('active');
         console.log('Two');
         break;
 
