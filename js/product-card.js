ab2e76507e4982b1a7df925bc9f1f47793264493
ExLuZiVe53 <imstudent27@gmail.com>
2025-05-18 11:59:24 +0200

    Update product-card.js

--- js/product-card.js
+++ js/product-card.js
@@ -1,31 +1,40 @@
 const activeTab = document.querySelector('.buttons-select');
-const activeButton = document.querySelectorAll('.button-select__tab');
+const activeButton = document.querySelector('.button-select__tab');
+console.log(activeButton);
 
 const dataActions = ['Description', 'Compound', 'Use'];
-const markup = dataActions
-  .map((actionName, index) => {
-    return `<button
-            class="buttons-select__tab" type="radio" data-action=${index}>
-            ${actionName}
-            </button>`;
-  })
-  .join('');
-activeTab.innerHTML = markup;
-// createButton();
 
-// function createButton() {
-//   const items = [];
-//   for (let i = 0; i < dataActions.length; i += 1) {
-//     const item = document.createElement('button');
-//     item.type = 'radio';
-//     item.classList.add('button-select__tab');
-//     item.style.width = '100px';
-//     item.style.height = '50px';
-//     item.style.margin = '10px';
-//     items.push(item);
-//   }
-//   activeTab.append(...items);
-// }
+// const markup = dataActions
+//   .map((actionName, index) => {
+//     return `<button
+//             class="button-select__tab" type="radio" data-action=${index}>
+//             ${actionName}
+//             </button>`;
+//   })
+//   .join('');
+
+// activeTab.innerHTML = markup;
+
+createButton();
+
+function createButton() {
+  const items = [];
+  for (let i = 0; i < dataActions.length; i += 1) {
+    const item = document.createElement('a');
+    item.role = 'button';
+    item.classList.add('button-select__tab');
+    item.style.width = '100px';
+    item.style.height = '50px';
+    item.style.margin = '10px';
+    item.style.border = '3px';
+    item.style.borderStyle = 'solid';
+    item.style.borderColor = 'lightblue';
+    item.textContent = dataActions[i];
+    item.dataset.action = i;
+    items.push(item);
+  }
+  activeTab.append(...items);
+}
 
 const buttonHandler = event => {
   switch (event.target.dataset.action) {
